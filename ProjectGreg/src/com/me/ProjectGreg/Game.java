3a5936f42e2466bc2cc67f898bb6cd4b7eae0e8f
Owner <brett@ubuntu.(none)>
2013-01-26 16:51:49 -0600

    merge

---
+++ ProjectGreg/src/com/me/ProjectGreg/Game.java
@@ -0,0 +1,99 @@
+package com.me.projectgreg;
+
+//import com.me.projectgreg.*;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.Color;
+
+public class Game implements ApplicationListener {
+	public class Task extends TimerTask 
+	{
+		@Override
+		public void run()
+		{
+			Heartbeat.INSTANCE().calmDown();
+		}
+	}
+	private OrthographicCamera camera;
+	SpriteBatch batch;
+	SpriteBatch playerBatch;
+	private Texture texture;
+	private Sprite sprite;
+	private Sprite playerSprite;
+	private Texture playerTexture;
+	private Platform []level;
+	private Player player;
+	
+	@Override
+	public void create() {		
+		float w = Gdx.graphics.getWidth();
+		float h = Gdx.graphics.getHeight();
+		level = new Platform[1];
+		player = new Player(200, 200);
+		for(int i = 0; i<level.length; i++)
+		{
+			level[i] = new Platform(this);
+		}
+		
+		camera = new OrthographicCamera(w, h);
+		batch = new SpriteBatch();
+		playerBatch = new SpriteBatch();
+		playerTexture = new Texture(Gdx.files.internal("data/Greg.png"));
+		texture = new Texture(Gdx.files.internal("data/platform.png"));
+		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+		
+		TextureRegion playerRegion = new TextureRegion(playerTexture, 64, 64);
+		TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
+		playerSprite = new Sprite(playerRegion);
+		player.setSprite(playerSprite);
+		sprite = new Sprite(region);
+		sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
+		sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
+		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
+	}
+
+	@Override
+	public void dispose() {
+		batch.dispose();
+		texture.dispose();
+		playerBatch.dispose();
+		playerTexture.dispose();
+	}
+
+	@Override
+	public void render() {		
+		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		//camera.translate(.01f,0f);
+		batch.setProjectionMatrix(camera.combined);
+		
+		batch.begin();
+		level[0].sprite().draw(batch);
+		//sprite.draw(batch);
+		batch.end();
+		playerBatch.begin();
+		player.sprite().draw(playerBatch);
+		player.update();
+		player.sprite().setPosition((float)player.getX(), (float)player.getY());
+		playerBatch.end();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
+	}
+}
